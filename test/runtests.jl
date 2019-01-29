e50efe9b39322d42ef77a42a73e98d184304eaa7
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2019-01-29 12:46:50 +0000

    fix test

--- test/runtests.jl
+++ test/runtests.jl
@@ -16,10 +16,10 @@ using SparseArrays, StructArrays
     @test cplx[1].simplices == StructArray([(1,), (2,), (3,), (4,)],)
     @test cplx[1].values == [0.0, 0.0, 0.0, 0.0]
 
-    @test cplx[2].simplices == StructArray([(1, 2), (1, 3), (2, 3)])
+    @test cplx[2].simplices == StructArray([(2, 1), (3, 1), (3, 2)])
     @test cplx[2].values == [0.2, 0.9, 0.5]
 
-    @test cplx[3].simplices == StructArray([(1, 2, 3)])
+    @test cplx[3].simplices == StructArray([(3, 2, 1)])
     @test cplx[3].values == [0.9]
 
     @test length(cplx) == 3
@@ -28,20 +28,35 @@ using SparseArrays, StructArrays
     @test cplx[1].simplices == StructArray([(1,), (2,), (3,), (4,)],)
     @test cplx[1].values == [0.0, 0.0, 0.0, 0.0]
 
-    @test cplx[2].simplices == StructArray([(1, 2), (1, 3), (2, 3)])
+    @test cplx[2].simplices == StructArray([(2, 1), (3, 1), (3, 2)])
     @test cplx[2].values == [0.2, 0.9, 0.5]
 
     @test length(cplx) == 2
 end
     
-using GaloisFields
-const GF = @GaloisField 3
-ts = -pi:0.5:pi
-pts = map(t -> [cos(t), sin(t)], ts)
+# using GaloisFields
+# const GF = @GaloisField 3
+# ts = [-pi, -pi/2, 0, pi/2]
+# pts = map(t -> [cos(t), sin(t)], ts)
 
-m = [norm(pt1-pt2) for pt1 in pts, pt2 in pts]
-cplx = vietorisrips(sparse(m), 2)
-sv = PersistentCohomology.simplexlist(cplx)
+# m = [norm(pt1-pt2) for pt1 in pts, pt2 in pts]
+# cplx = vietorisrips(sparse(m), 2)
+# sv = PersistentCohomology.simplexlist(cplx)
 
-PersistentCohomology.persistent_cocycles(GF, cplx, sv, 1)
+# PersistentCohomology.persistent_cocycles(GF, cplx, sv, 1)
+
+# s = spzeros(4, 4)
+# s[1, 2] = 0.3
+# s[2, 3] = 0.4
+# s[3, 4] = 0.5
+# s[1, 4] = 0.3
+# s[1, 3] = 0.7
+# s[2, 4] = 0.8
+
+
+
+# cplx = vietorisrips(s, 2)
+# sv = PersistentCohomology.simplexlist(cplx)
+
+# PersistentCohomology.persistent_cocycles(F, cplx, sv, 1)
 
