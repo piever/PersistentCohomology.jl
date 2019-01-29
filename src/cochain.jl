8411fca5abe1b6608eee9effaa1f367236321430
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2019-01-29 15:51:03 +0000

    Implement persistent cocycle (#2)

--- src/cochain.jl
+++ src/cochain.jl
@@ -6,16 +6,7 @@ end
 Base.length(c::Cochain) = length(c.values)
 Base.getindex(s::Cochain, t::Tuple) = s.values[searchsortedfirst(s.simplices, t)]
 
-# From IndexedTables to get similar functionality as NDSparse
-Base.broadcast(f::Function, s::Cochain, t::Cochain) = Cochain(s.simplices, broadcast(f, s.values, t.values))
-Base.broadcast(f::Function, x::Cochain) = Cochain(x.simplices, broadcast(f, x.values))
-Base.broadcast(f::Function, x::Cochain, y) = Cochain(x.simplices, broadcast(f, x.values, y))
-Base.broadcast(f::Function, y, x::Cochain) = Cochain(x.simplices, broadcast(f, y, x.values))
-
-Broadcast.broadcasted(f::Function, A::Cochain) = broadcast(f, A)
-Broadcast.broadcasted(f::Function, A::Cochain, B::Cochain) = broadcast(f, A, B)
-Broadcast.broadcasted(f::Function, A, B::Cochain) = broadcast(f, A, B)
-Broadcast.broadcasted(f::Function, A::Cochain, B) = broadcast(f, A, B)
+Cochain(c::Cochain, v::AbstractVector) = Cochain(c.simplices, v)
 
 function face(s, i)
     Tuple(val for (ind, val) in enumerate(s) if ind != i)
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,11 @@
 using PersistentCohomology
+using PersistentCohomology: Span
+
 using Test
 
 using SparseArrays, StructArrays
+using LinearAlgebra
+using GaloisFields
 
 @testset "vietorisrips" begin
 
@@ -16,10 +20,10 @@ using SparseArrays, StructArrays
     @test cplx[1].simplices == StructArray([(1,), (2,), (3,), (4,)],)
     @test cplx[1].values == [0.0, 0.0, 0.0, 0.0]
 
-    @test cplx[2].simplices == StructArray([(1, 2), (1, 3), (2, 3)])
+    @test cplx[2].simplices == StructArray([(2, 1), (3, 1), (3, 2)])
     @test cplx[2].values == [0.2, 0.9, 0.5]
 
-    @test cplx[3].simplices == StructArray([(1, 2, 3)])
+    @test cplx[3].simplices == StructArray([(3, 2, 1)])
     @test cplx[3].values == [0.9]
 
     @test length(cplx) == 3
@@ -28,10 +32,41 @@ using SparseArrays, StructArrays
     @test cplx[1].simplices == StructArray([(1,), (2,), (3,), (4,)],)
     @test cplx[1].values == [0.0, 0.0, 0.0, 0.0]
 
-    @test cplx[2].simplices == StructArray([(1, 2), (1, 3), (2, 3)])
+    @test cplx[2].simplices == StructArray([(2, 1), (3, 1), (3, 2)])
     @test cplx[2].values == [0.2, 0.9, 0.5]
 
     @test length(cplx) == 2
 end
     
+@testset "persistent_cocycles" begin
+    GF = @GaloisField 3
+    ts = [-pi, -pi/2, 0, pi/2]
+    pts = map(t -> [cos(t), sin(t)], ts)
 
+    m = round.([norm(pt1-pt2) for pt1 in pts, pt2 in pts], digits = 4)
+    cplx = vietorisrips(sparse(m), 2)
+    @test cplx[1].simplices == StructArray(([1, 2, 3, 4],))
+    @test cplx[2].simplices == StructArray([(2, 1), (3, 1), (3, 2), (4, 1), (4, 2), (4, 3)])
+    @test cplx[3].simplices == StructArray([(3, 2, 1), (4, 2, 1), (4, 3, 1), (4, 3, 2)])
+
+    sv = PersistentCohomology.simplexlist(cplx)
+
+    short = m[1, 2]
+    long = m[1, 3]
+
+    cocycles = PersistentCohomology.persistent_cocycles(GF, cplx, sv, 1)
+
+    @test cocycles[1].span[1] == Span{Float64}(0, Inf)
+    @test findnz(cocycles[1].cocycle[1].values) == ([1, 2, 3, 4], GF.([1, 1, 1, 1]))
+    @test cocycles[1].span[2] == Span{Float64}(0, short)
+    @test findnz(cocycles[1].cocycle[2].values) == ([2], GF.([1]))
+    @test cocycles[1].span[3] == Span{Float64}(0, short)
+    @test findnz(cocycles[1].cocycle[3].values) == ([3], GF.([1]))
+    @test cocycles[1].span[4] == Span{Float64}(0, short)
+    @test findnz(cocycles[1].cocycle[4].values) == ([4], GF.([1]))
+    @test length(cocycles[1]) == 4
+
+    @test cocycles[2].span[1] == Span{Float64}(short, long)
+    @test findnz(cocycles[2].cocycle[1].values) == ([6], GF.([1]))
+    @test length(cocycles[2]) == 1
+end
